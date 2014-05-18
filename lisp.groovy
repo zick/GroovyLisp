51e9f00a24da1a6d727ffc126a213e97440cf33d
zick <zickminoh@gmail.com>
2014-05-18 13:09:50 +0900

    Add readList and printObj.

--- lisp.groovy
+++ lisp.groovy
@@ -44,7 +44,18 @@ def makeSubr(fn) {
 }
 
 def makeExpr(args, env) {
-  return [tag: "expr", args: safeCar(args), body: safeCdr(args), env: env]
+  return [tag: 'expr', args: safeCar(args), body: safeCdr(args), env: env]
+}
+
+def nreverse(lst) {
+  def ret = kNil
+  while (lst['tag'] == 'cons') {
+    def tmp = lst['cdr']
+    lst['cdr'] = ret
+    ret = lst
+    lst = tmp
+  }
+  return ret
 }
 
 def isDelimiter(c) {
@@ -63,7 +74,7 @@ def makeNumOrSym(str) {
 }
 
 def readAtom(str) {
-  next = ''
+  def next = ''
   for (i = 0; i < str.size(); i++) {
     if (isDelimiter(str[i])) {
       next = str[i..-1]
@@ -74,20 +85,81 @@ def readAtom(str) {
   return [makeNumOrSym(str), next]
 }
 
+def rest(str, i) {
+  // rest('a', 1) => '' ('a'[1..-1] causes an out of range error)
+  if (str.size() <= i) {
+    return ''
+  }
+  return str[i..-1]
+}
+
 def read(str) {
   str = skipSpaces(str)
   if (str == '') {
-    return makeError('empty input')
+    return [makeError('empty input'), '']
   } else if (str[0] == kRPar) {
-    return makeError('invalid syntax: ' + str)
+    return [makeError('invalid syntax: ' + str), '']
   } else if (str[0] == kLPar) {
-    return makeError('noimpl')
+    return readList(rest(str, 1))
   } else if (str[0] == kQuote) {
-    return makeError('noimpl')
+    def tmp = read(rest(str, 1))
+    return [makeCons(makeSym('quote'), makeCons(tmp[0], kNil)), tmp[1]]
   }
   return readAtom(str)
 }
 
+def readList(str) {
+  def ret = kNil
+  while (true) {
+    str = skipSpaces(str)
+    if (str == '') {
+      return [makeError('unfinished parenthesis'), '']
+    } else if (str[0] == kRPar) {
+      break
+    }
+    def tmp = read(str)
+    def elm = tmp[0]
+    def next = tmp[1]
+    if (elm['tag'] == 'error') {
+      return [elm, '']
+    }
+    ret = makeCons(elm, ret)
+    str = next
+  }
+  return [nreverse(ret), rest(str, 1)]
+}
+
+def printObj(obj) {
+  if (obj['tag'] == 'num' || obj['tag'] == 'sym' || obj['tag'] == 'nil') {
+    return obj['data'].toString()
+  } else if (obj['tag'] == 'error') {
+    return '<error: ' + obj['data'] + '>'
+  } else if (obj['tag'] == 'cons') {
+    return printList(obj)
+  } else if (obj['tag'] == 'subr' || obj['tag'] == 'expr') {
+    return '<' + obj['tag'] + '>'
+  }
+  return '<unknown>'
+}
+
+def printList(obj) {
+  def ret = ''
+  def first = true
+  while (obj['tag'] == 'cons') {
+    if (first) {
+      first = false
+    } else {
+      ret += ' '
+    }
+    ret += printObj(obj['car'])
+    obj = obj['cdr']
+  }
+  if (obj['tag'] == 'nil') {
+    return '(' + ret + ')'
+  }
+  return '(' + ret + ' . ' + printObj(obj) + ')'
+}
+
 while (line = System.console().readLine('> ')) {
-  println(read(line))
+  println(printObj(read(line)[0]))
 }
